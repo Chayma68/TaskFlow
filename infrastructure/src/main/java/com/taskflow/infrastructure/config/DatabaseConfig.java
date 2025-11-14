f8ba33445ccd99e9d2baeb03ddcd056ed096ae97
Chayma Chliyah <chliyahchaimae2003@gmail.com>
2025-11-14 22:20:39 +0100

    added Infrastructure

---
+++ infrastructure/src/main/java/com/taskflow/infrastructure/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package com.taskflow.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+    // Ici tu pourrais ajouter des customisations si nécessaire.
+}
