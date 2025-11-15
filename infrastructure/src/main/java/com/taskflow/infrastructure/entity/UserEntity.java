e5daf86c5e6aaff2e63aa0a53996f8863ba6bc5f
Chayma Chliyah <chliyahchaimae2003@gmail.com>
2025-11-15 22:01:30 +0100

    Added UserEntity

---
+++ infrastructure/src/main/java/com/taskflow/infrastructure/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.taskflow.infrastructure.entity;
+
+import com.taskflow.model.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
